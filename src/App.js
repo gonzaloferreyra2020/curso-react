bea7288cc4a5092f811041bbce76ed8b7414be56
gonzaloferreyra2020 <gonzalof.188@gmail.com>
2022-08-27 21:55:40 -0300

    Componente Cart y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Carrousel from './components/Carrousel/Carrousel';
 import Cupones from './components/Cupones/Cupones';
 import Footer from './components/Footer/Footer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/Navbar/Navbar';
 import PromoApp from './components/PromoApp/PromoApp';
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='fondo_principal'>
       <Navbar />
+      <ItemListContainer/>
       <Carrousel />
       <Cupones />
       <PromoApp/>
