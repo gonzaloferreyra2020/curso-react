2efb490706dad72b594571a7ff0cb6e621bb074e
gonzaloferreyra2020 <gonzalof.188@gmail.com>
2022-09-14 02:18:21 -0300

    Routing

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,12 +4,12 @@ import Item from "../Item/Item";
 const ItemList = (props)=>{
     const{items}=props;
     return(
-        <div className="row ">
-            < div className = "col-sm-6 col-md-4 col-lg-3 mt-4">
-                <div className="card-center">
+        <div className="estilo-productos ">
+           
+                <div>
                     {items.map(product => <Item key={product.id} nombre={product.title} precio={product.price} imagen={product.image} stock={product.stock}/>)}
                 </div>
-                </div>
+                
         </div>
     )
 }
